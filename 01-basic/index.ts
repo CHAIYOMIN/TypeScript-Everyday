5089a8e4180b8de158970ed08f0c9416bff09917
CHAI YOMIN <chaiyomin@LAB-N2123.local>
2022-04-20 19:08:12 +0900

    first day -2

---
+++ 01-basic/index.ts
@@ -0,0 +1,25 @@
+// index.ts
+let myName = 'Maxwell';
+let age = 20;
+let hasPet = false;
+let nothing = undefined;
+let nothingLiterally = null;
+
+// typescript 型別推論（Type Inference）
+// ex: myName age hasPet 會被推論為上一個設定的型別
+// ex: nothing nothingLiterally 就會被推論為 any type 
+
+//作者提到 any型別 容易容易造成混亂～
+
+//遲滯性指派 Delayed Initialization
+// ex
+let hello;
+
+hello = "hello";
+hello = 123;
+
+//typescript 就不會鳥你啦
+
+//declare let name
+// 如何在typescript中解决 error TS2451: Cannot redeclare block-scoped variable 'name'
+// https://www.jianshu.com/p/78268bd9af0a 

b1457d19dbe0ed585dbd08221d0a0e38c011107c
CHAI YOMIN <chaiyomin@LAB-N2123.local>
2022-05-11 18:11:24 +0900

    typscript udemy backup

---
+++ typescript_udemy/basic/test.ts
@@ -0,0 +1,14 @@
+const book: [string, number ,boolean] = ["hello", 1500,false];
+
+enum CoffeeSize {
+    SHOT = "SHORT",
+    TALL = "TALL"
+}
+
+const coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+}
+
+// coffee size can only be used in enum setting
+
